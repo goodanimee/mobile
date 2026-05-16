981ed264f0bcec529b7b01d5792329deea5f4646
Harmeet Singh Bhatia <h@thedrone7.dev>
2026-05-17 03:43:57 +0530

    feat(backend): add media list entry domain model

---
+++ backend/models/media_list_entry.go
@@ -0,0 +1,27 @@
+package models
+
+// MediaListEntry represents a media list entry
+type MediaListEntry struct {
+	ID          int32     `json:"id"`
+	Status      string    `json:"status"`
+	Progress    int32     `json:"progress"`
+	Score       float64   `json:"score"`
+	Repeat      int32     `json:"repeat"`
+	StartedAt   FuzzyDate `json:"startedAt"`
+	CompletedAt FuzzyDate `json:"completedAt"`
+}
+
+// SaveMediaListEntryDTO represents the DTO for saving a list entry
+type SaveMediaListEntryDTO struct {
+	SaveMediaListEntry MediaListEntry `json:"SaveMediaListEntry"`
+}
+
+// DeleteMediaListEntry represents the response when deleting a list entry
+type DeleteMediaListEntry struct {
+	Deleted bool `json:"deleted"`
+}
+
+// DeleteMediaListEntryDTO represents the DTO for deleting a list entry
+type DeleteMediaListEntryDTO struct {
+	DeleteMediaListEntry DeleteMediaListEntry `json:"DeleteMediaListEntry"`
+}
